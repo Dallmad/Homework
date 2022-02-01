dd62e30bb4d57111d312234292b8f2efed9640a6
Dzmitry <maddal.privod@gmail.com>
2022-02-01 12:17:31 +0300

    homework 03 app

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import s from './App.module.css'
-import HW2 from '../../../p2-homeworks/h2/HW2'
+import HW3 from '../../../p2-homeworks/h3/HW3'
 
 function App() {
     return (
         <div className={s.App}>
-            <div>react homeworks: 02</div>
+            <div>react homeworks: 03</div>
             {/*<HW1/>*/}
-            <HW2/>
-            {/*<HW3/>*/}
+            {/*<HW2/>*/}
+            <HW3/>
             {/*<HW4/>*/}
             {/*<HW5/>*/}
 
