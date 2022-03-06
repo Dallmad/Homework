30d1cb23931b53500f59c0afb8ca74db32cf2b8f
Dzmitry <maddal.privod@gmail.com>
2022-03-06 18:06:56 +0300

    homework 07 ready

---
+++ src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
@@ -0,0 +1,36 @@
+import React, {SelectHTMLAttributes, DetailedHTMLProps, ChangeEvent} from 'react'
+
+type DefaultSelectPropsType = DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>
+
+type SuperSelectPropsType = DefaultSelectPropsType & {
+    options?: string[]
+    onChangeOption?: (option: string) => void
+}
+
+const SuperSelect: React.FC<SuperSelectPropsType> = (
+    {
+        options,
+        onChange, onChangeOption,
+        ...restProps
+    }
+) => {
+    // const mappedOptions: any[] = []; // map options with key
+    const mappedOptions: any[] = options ? options.map((o, i) => ( // map options with key
+        <option key={o+'-'+i} value={o}>{o}</option>
+    )):[]
+
+    const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
+        onChange && onChange(e)
+        onChangeOption && onChangeOption(e.currentTarget.value)
+
+        // onChange, onChangeOption
+    }
+
+    return (
+        <select onChange={onChangeCallback} {...restProps}>
+            {mappedOptions}
+        </select>
+    )
+}
+
+export default SuperSelect
